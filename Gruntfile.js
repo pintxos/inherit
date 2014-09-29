898bc19e58d50dd9053b715b02b0ecf804d39b9a
Jelle Versele <jelle@versele.be>
2014-09-29 22:02:02 +0200

    New grunt setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,10 @@
-module.exports = function (grunt) {
-
-	'use strict';
-
-	require('load-grunt-tasks')(grunt);
-
-	grunt.initConfig({
-
-		karma: {
-			options: {
-				basePath: '',
-				files: [
-					'index.js',
-					'test/*.js'
-				],
-				frameworks: [
-					'jasmine'
-				]
-			},
-			dev: {
-				browsers: ['Chrome']
-			},
-			ci: {
-				browsers: ['PhantomJS'],
-				singleRun: true
-			}
-		},
-
-		jshint: {
-			files: ['*.js'],
-			options: {
-				strict: true,
-				es3: true,
-				globals: {
-					window: true,
-					define: true
-				}
-			}
-		}
-	});
-
-	grunt.registerTask('default', []);
-	grunt.registerTask('test', ['jshint', 'karma:dev']);
-	grunt.registerTask('testCI', ['jshint', 'karma:ci']);
-
+var config = {
+	testDependencies: [
+		'index.js',
+		'test/*.js'
+	]
 };
+
+module.exports = require('grunt-pintxos')(config);
+
+
